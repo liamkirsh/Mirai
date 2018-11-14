9752d11f213d2c25c94d66e309081b18710535d3
Liam Kirsh <liam@cmu.edu>
2018-11-14 23:34:12 +0000

    update run script to set up db

---
+++ create.sql
@@ -0,0 +1,1 @@
+create table bots(ip string, int port, uname string, pword string);
--- run.py
+++ run.py
@@ -1,2 +1,5 @@
+import os
 from app import app
+
+os.system("rm -f mirai.db; sqlite3 mirai.db < create.sql")
 app.run(host="0.0.0.0")
